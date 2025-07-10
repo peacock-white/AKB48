5f039e7bc09018bf3b20ab407073f4929b4ae677
peacock-white <katagami.hashiranokai@gmail.com>
2025-07-10 17:22:44 +0900

    Add files via upload

---
+++ single.js
@@ -0,0 +1,27 @@
+'use strict';
+
+{
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      // type: 'fraction',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+
+
+
+
+}
