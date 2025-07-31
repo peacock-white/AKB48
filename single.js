1ccfe6d658c14b36ce0eafc18c4c4c6b48b861f3
peacock-white <katagami.hashiranokai@gmail.com>
2025-07-31 22:30:13 +0900

    Add files via upload

--- single.js
+++ single.js
@@ -2,22 +2,59 @@
 
 {
 
-  const swiper = new Swiper('.swiper', {
+  const swiper1 = new Swiper('.swiper1', {
     loop: true,
+    loopAdditionalSlides: 1,
+    centeredSlides: true,
     grabCursor: true,
+    autoplay: {
+      delay: 1000,
+    },
+    // effect: 'fade',
+    speed: 1000,
     navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next',
+      prevEl: '.my-button-prev',
+      nextEl: '.my-button-next',
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      type: 'fraction',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    //   type: 'fraction',
+    // },
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    watchSlidesProgress: true,
   });
+
+ const swiper2 = new Swiper('.swiper2', {
+    loop: true,
+    loopAdditionalSlides: 1,
+    centeredSlides: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 0,
+    },
+    speed: 3000,
+   
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    //   type: 'fraction',
+    // },
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    watchSlidesProgress: true,
+  });
+
+
+
+
+
+
+
+
 
 
 
